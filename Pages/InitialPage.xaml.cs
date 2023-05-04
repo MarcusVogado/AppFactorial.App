4970ff3401915143ff7e03b73740abb80bdad8da
marcus vogado <marcus_620@hotmail.com>
2023-05-04 14:57:55 -0300

    update, Testes Icon SplashScreen

--- Pages/InitialPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace AppFactorial.Pages;
-
-public partial class InitialPage : Shell
-{
-	public InitialPage()
-	{
-		InitializeComponent();
-	}
-}
---
+++ Themes/LightTheme.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppFactorial.Themes;
+
+public partial class LightTheme : ResourceDictionary
+{
+	public LightTheme()
+	{
+		InitializeComponent();
+	}
+}
